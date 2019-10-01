a15d02069bf42e60214671031ebefd69ff81694c
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2019-10-01 12:50:17 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-// CAUGHT SPEEDING ASSIGNMENT START CODE
-
-// HTML Elements
-let calcBtnEl = document.getElementById('calc-btn');
-let speedEl = document.getElementById('speed');
-let limitEl = document.getElementById('limit');
-let birthdayEl = document.getElementById('birthday');
-let ticketEl = document.getElementById('ticket-type');
-let decisionEl = document.getElementById('decision');
+// CAUGHT SPEEDING ASSIGNMENT SOLUTION
 
 // Add Event Listener
-calcBtnEl.addEventListener('click', caughtSpeeding);
+document.getElementById('calc-btn').addEventListener('click', caughtSpeeding);
 
 // Event Function
 function caughtSpeeding() {
-    // Inputs
-    let speedInput = Number(speedEl.value);
-    let limitInput = Number(limitEl.value);
-    let birthdayInput = birthdayEl.value;
+    // Input Values
+    let speedInput = Number(document.getElementById('speed').value);
+    let limitInput = Number(document.getElementById('limit').value);
+    let birthdayInput = document.getElementById('birthday').value;
 
-    // Determine Result
+    // HTML Output Elements
+    let ticketEl = document.getElementById('ticket-type');
+    let decisionEl = document.getElementById('decision');
+
+    // Determine and Output Ticket Result
     if (isInputValid(speedInput, limitInput)) {
         ticketEl.innerHTML = determineTicketType(speedInput, limitInput);
-        decisionEl.innerHTML = determineDecision(birthdayInput)
+        decisionEl.innerHTML = determineDecision(birthdayInput);
+    } else {
+        alert("Car speed must be greater than speed limit.")
+        ticketEl.innerHTML = '-----';
+        decisionEl.innerHTML = '-----';
     }
     
 }
